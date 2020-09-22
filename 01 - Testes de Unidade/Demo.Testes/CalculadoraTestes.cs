6a4dc3d34ecd26e4b8dc44f90ab3fe25453e9f61
Wellington <wellingtonti23@gmail.com>
2020-09-22 08:30:26 -0400

    Adicionado projeto Demo

---
+++ 01 - Testes de Unidade/Demo.Testes/CalculadoraTestes.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Demo.Testes
+{
+    public class CalculadoraTestes
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(4, 2);
+
+            // Assert
+            Assert.Equal(6, resultado);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 5, 7)]
+        [InlineData(10, 52, 62)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double valor1, double valor2, double totalEsperado)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(valor1, valor2);
+
+            // Assert
+            Assert.Equal(totalEsperado, resultado);
+        }
+    }
+}
---
+++ 01 - Testes de Unidade/Demo/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double valor1, double valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
