4a4e9be9e9312bba185d3c5fd97805b084a7a846
Wellington <wellingtonti23@gmail.com>
2020-09-22 22:00:04 -0400

    Adicionando classes de assert(asserções)

---
+++ 01 - Testes de Unidade/Demo.Testes/AssertNumbersTestes.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Demo.Testes
+{
+    public class AssertNumbersTestes
+    {
+        [Fact]
+        public void Calculadora_Somar_DeveSerIgual()
+        {
+            var calculadora = new Calculadora();
+
+            var resultado = calculadora.Somar(1, 2);
+
+            Assert.Equal(3, resultado);
+        }
+
+        [Fact]
+        public void Calculadora_Somar_NaoDeveSerIgual()
+        {
+            var calculadora = new Calculadora();
+
+            var resultado = calculadora.Somar(1.18, 2.25464654);
+
+            Assert.NotEqual(3.3, resultado, 1);
+        }
+    }
+}
---
+++ 01 - Testes de Unidade/Demo.Testes/AssertStringsTestes.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+namespace Demo.Testes
+{
+    public class AssertStringsTestes
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.Equal("Wellington Carvalho", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.Equal("WELLINGTON CARVALHO", nomeCompleto, true);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.Contains("gton Car", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.StartsWith("Well", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.EndsWith("lho", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaRegular()
+        {
+            var stringsTools = new StringsTools();
+
+            var nomeCompleto = stringsTools.Unir("Wellington", "Carvalho");
+
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
