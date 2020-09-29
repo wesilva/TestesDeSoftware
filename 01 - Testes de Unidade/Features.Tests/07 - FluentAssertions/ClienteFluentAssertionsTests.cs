6a7206a5a28f284042c72518376f14e807d7e359
Wellington <wellingtonti23@gmail.com>
2020-09-28 21:21:28 -0400

    Adicionando Fluent Assertion

---
+++ 01 - Testes de Unidade/Features.Tests/07 - FluentAssertions/ClienteFluentAssertionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Features.Clientes;
+using FluentAssertions;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteAutoMockerColletion))]
+    public class ClienteFluentAssertionsTests
+    {
+        private readonly ClienteTestsAutoMockerFixture _clienteTestsAutoMockerFixture;
+        private readonly ClienteService _clienteService;
+
+        public ClienteFluentAssertionsTests(ClienteTestsAutoMockerFixture clienteTestsAutoMockerFixture)
+        {
+            _clienteTestsAutoMockerFixture = clienteTestsAutoMockerFixture;
+            _clienteService = _clienteTestsAutoMockerFixture.ObterClienteService();
+        }
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Fluent Assertion Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            var cliente = _clienteTestsAutoMockerFixture.GerarClienteValido();
+
+            var result = cliente.EhValido();
+
+            //Assert.True(result);
+            //Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+            result.Should().BeTrue();
+            cliente.ValidationResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Fluent Assertion Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            var cliente = _clienteTestsAutoMockerFixture.GerarClienteInvalido();
+
+            var result = cliente.EhValido();
+
+            //Assert.False(result);
+            //Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+
+            result.Should().BeFalse();
+            cliente.ValidationResult.Errors.Should().HaveCountGreaterOrEqualTo(1, "Deve possuir erros de validação");
+        }
+    }
+}
---
+++ 01 - Testes de Unidade/Features.Tests/07 - FluentAssertions/ClienteServiceFluentAssertionTests.cs
@@ -0,0 +1,75 @@
+﻿using Features.Clientes;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using MediatR;
+using Moq;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteAutoMockerColletion))]
+    public class ClienteServiceFluentAssertionTests
+    {
+        private readonly ClienteTestsAutoMockerFixture _clienteTestsAutoMockerFixture;
+        private readonly ClienteService _clienteService;
+
+        public ClienteServiceFluentAssertionTests(ClienteTestsAutoMockerFixture clienteTestsAutoMockerFixture)
+        {
+            _clienteTestsAutoMockerFixture = clienteTestsAutoMockerFixture;
+            _clienteService = _clienteTestsAutoMockerFixture.ObterClienteService();
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Fluent Assertion Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            var cliente = _clienteTestsAutoMockerFixture.GerarClienteValido();
+
+            _clienteService.Adicionar(cliente);
+
+            //Assert.True(cliente.EhValido());
+            cliente.EhValido().Should().BeTrue();
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Once);
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Fluent Assertion Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            var cliente = _clienteTestsAutoMockerFixture.GerarClienteInvalido();
+
+            _clienteService.Adicionar(cliente);
+
+            cliente.EhValido().Should().BeFalse("Possui inconsistências");
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Never);
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Fluent Assertion Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IClienteRepository>()
+                .Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsAutoMockerFixture.ObterClientesVariados());
+
+            var clientes = _clienteService.ObterTodosAtivos();
+
+            //Assert
+            //Assert.True(clientes.Any());
+            //Assert.False(clientes.Count(c => !c.Ativo) > 0);
+
+            //Assert Fluent
+            clientes.Should().HaveCountGreaterOrEqualTo(1).And.OnlyHaveUniqueItems();
+            clientes.Should().NotContain(c => !c.Ativo);
+            _clienteTestsAutoMockerFixture.AutoMocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            _clienteService.ExecutionTimeOf(c => c.ObterTodosAtivos())
+                .Should()
+                .BeLessOrEqualTo(50.Milliseconds(),
+                    "É executado milhares de vezes por segundo!");
+        }
+    }
+}
+
