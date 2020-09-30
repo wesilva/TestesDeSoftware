9f355b48b397a0fa2925f8ce97b2169b56b83935
Wellington <wellingtonti23@gmail.com>
2020-09-30 17:48:42 -0400

    Pedido - Adicionar item - parte 1 Adiciondo snippet

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+        public decimal ValorTotal { get; private set; }
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems.AsReadOnly();
+
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItems.Add(pedidoItem);
+            ValorTotal = PedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+        }
+    }
+
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 5, 500);
+
+            pedido.AdicionarItem(pedidoItem);
+
+            Assert.Equal(2500, pedido.ValorTotal);
+        }
+    }
+}
