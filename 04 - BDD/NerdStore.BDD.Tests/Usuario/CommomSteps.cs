bfc5d8491710f1ef329873fd15050653ba197c4f
Wellington <wellingtonti23@gmail.com>
2020-10-15 08:00:38 -0400

    Gerando testes de comportamento para os cenários de cadastro  e login de usuários

---
+++ 04 - BDD/NerdStore.BDD.Tests/Usuario/CommomSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace NerdStore.BDD.Tests.Usuario
+{
+    [Binding]
+    public class CommomSteps
+    {
+        [Given(@"Que o visitante está acessando o site da loja")]
+        public void DadoQueOVisitanteEstaAcessandoOSiteDaLoja()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Ele será redirecionado para a vitrine")]
+        public void EntaoEleSeraRedirecionadoParaAVitrine()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Uma saudação com seu e-mail será exibida no menu superior")]
+        public void EntaoUmaSaudacaoComSeuE_MailSeraExibidaNoMenuSuperior()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ 04 - BDD/NerdStore.BDD.Tests/Usuario/LoginDeUsuariosSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace NerdStore.BDD.Tests.Usuario
+{
+    [Binding]
+    public class LoginDeUsuariosSteps
+    {
+        [When(@"Ele clicar em login")]
+        public void QuandoEleClicarEmLogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Preencher os dados do formulário de login")]
+        public void QuandoPreencherOsDadosDoFormularioDeLogin(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Clicar no botão login")]
+        public void QuandoClicarNoBotaoLogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
