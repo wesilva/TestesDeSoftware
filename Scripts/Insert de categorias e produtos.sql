8e97b2cc98908968eea7541a71295a3a43a5e953
Wellington <wellingtonti23@gmail.com>
2020-10-18 12:06:21 -0400

    Adicionando feature do catalogo e script de inserção de dados

---
+++ Scripts/Insert de categorias e produtos.sql
@@ -0,0 +1,15 @@
+-- Categoria
+-- INSERT INTO Categorias VALUES ('191DDD3E-ACD4-4C3B-AE74-8E473993C5DA', 'Camisetas', 1)
+-- INSERT INTO Categorias VALUES ('191ddd3e-acd4-4c3b-ae74-8e473993c8cc', 'Canecas', 2)
+
+-- Produto
+-- INSERT INTO Produtos VALUES('111ddd3e-acd4-4c3b-ae74-8e473993c5dc', '191DDD3E-ACD4-4C3B-AE74-8E473993C5DA', 'Camiseta Software Developer','Camiseta 100% algodão, resistente a lavagens e altas temperaturas', 1, 80, GETDATE(), 'camiseta1.jpg', 30, 50, 50, 20)
+-- INSERT INTO Produtos VALUES('111ddd3e-acd4-4c3b-ae74-8e473993c5db', '191DDD3E-ACD4-4C3B-AE74-8E473993C5DA', 'Camiseta Code Life Cinza','Camiseta 100% algodão, resistente a lavagens e altas temperaturas', 1, 100, GETDATE(), 'camiseta3.jpg', 30, 50, 50, 20)
+-- INSERT INTO Produtos VALUES('111ddd3e-acd4-4c3b-ae74-8e473993c5da', '191DDD3E-ACD4-4C3B-AE74-8E473993C5DA', 'Camiseta Debugar Preta','Camiseta 100% algodão, resistente a lavagens e altas temperaturas', 1, 110, GETDATE(), 'camiseta4.jpg', 30, 50, 50, 20)
+-- INSERT INTO Produtos VALUES('111ddd3e-acd4-4c3b-ae74-8e473993c5dd', '191DDD3E-ACD4-4C3B-AE74-8E473993C5DA', 'Camiseta Code Life Preta','Camiseta 100% algodão, resistente a lavagens e altas temperaturas', 1, 90, GETDATE(), 'camiseta2.jpg', 10, 50, 50, 20)
+   
+-- INSERT INTO Produtos VALUES('191ddd3e-acd4-4c3b-ae74-8e473993c5da', '191ddd3e-acd4-4c3b-ae74-8e473993c8cc', 'Caneca No Coffe No Code','Caneca de porcelana com impressão térmica', 1, 15, GETDATE(), 'caneca4.jpg', 18, 10, 11, 15)
+-- INSERT INTO Produtos VALUES('11bddd3e-acd4-4c3b-ae74-8e473993c5db', '191ddd3e-acd4-4c3b-ae74-8e473993c8cc', 'Caneca Star Bugs Coffee','Caneca de porcelana com impressão térmica', 1, 15, GETDATE(), 'caneca1.jpg', 20, 10, 11, 15)
+-- INSERT INTO Produtos VALUES('11bddd3e-acd4-4c3b-ae74-8e473993c5dc', '191ddd3e-acd4-4c3b-ae74-8e473993c8cc', 'Caneca Programmer Code','Caneca de porcelana com impressão térmica', 1, 10, GETDATE(), 'caneca2.jpg', 25, 10, 11, 15)
+-- INSERT INTO Produtos VALUES('11bddd3e-acd4-4c3b-ae74-8e473993c5dd', '191ddd3e-acd4-4c3b-ae74-8e473993c8cc', 'Caneca Turn Coffee in Code','Caneca de porcelana com impressão térmica', 1, 20, GETDATE(), 'caneca2.jpg', 20, 10, 11, 15)
+
